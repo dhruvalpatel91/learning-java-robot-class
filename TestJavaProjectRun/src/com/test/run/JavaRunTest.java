c64d26e568015ccdcd20b1837d1721c27568256d
dhruvalpatel91 <smart.dhru11@gmail.com>
2025-03-27 09:59:52 +0530

    Mouse effect using robot class

---
+++ TestJavaProjectRun/src/com/test/run/JavaRunTest.java
@@ -0,0 +1,25 @@
+package com.test.run;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.Random;
+
+public class JavaRunTest {
+	public static final int TIME = 5000;
+	public static final int MAX_Y = 400;
+	public static final int MAX_X = 400;
+
+	public static void main(String[] args) throws InterruptedException, AWTException {
+	
+		
+		// TODO Auto-generated method stub
+		Robot robot = new Robot();
+		Random random = new Random();while(true)
+		{
+			robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
+			Thread.sleep(TIME);
+		}
+	}
+	}
+
+
